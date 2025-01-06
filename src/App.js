fdfd669329109b870cfa4a7c5545421bffc5ceaf
InderN10 <midtulah2000@gmail.com>
2025-01-06 12:19:00 +0800

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
 
 import './App.css';
-
+import react, { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+const todo = {
+  text: "do homework",
+  id: 1,
+  status: "ACTIVE" | "COMPLETED"
+}
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("")
+  const [filterState, setFilterState] = useState("ALL")
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  };
+  function handleAddButton() {
+    if (inputValue.length === 0) {
+      setError("please enter task");
+      return;
+    } else {
+      setError("")
+      setTodo([...todo, { text: inputValue, id: uuidv4(), status: "ACTIVE" }]);
+      setInputValue("");
+    }
+  };
+  function handleCheckBox(id) {
+    const newTodos = todo.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, status: todo.status === "DONE" ? "ACTIVE" : "DONE" }
+      }
+      else {
+        return todo;
+      }
+    });
+    setTodo(newTodos);
+    console.log(todo);
+
+  };
+  function handleFilterState(state) {
+    setFilterState(state)
+  }
+
+  console.log(todo);
+
+
   return (
     <div className="App">
       <div id='BODY'>
         <p id='title'>To do list</p>
-        <div id='containerCLASSIFYING'>
-          <input id='writeTASK'></input>
-          <button id='addBUTTON'>Add</button>
-        </div>
         <div id='container'>
-          <button id='allTASKS'>All</button>
-          <button id='activeTASKS'>Active</button>
-          <button id='comletedTASKS'>Completed</button>
+          <input placeholder='write here' value={inputValue} onChange={handleInputChange} id='writeTASK'></input>
+          <button onClick={handleAddButton} id='addBUTTON'>Add</button>
+          {error.length > 1 && <div id='error'>{error}</div>}
         </div>
+        <div id='containerCLASSIFYING'>
+          <button onClick={() => handleFilterState("ALL")} id='allTASKS'>All</button>
+          <button onClick={() => handleFilterState("ACTIVE")} id='activeTASKS'>Active</button>
+          <button onClick={() => handleFilterState("DONE")} id='comletedTASKS'>Completed</button>
+        </div>
+        {todo.filter((todo) => {
+          if (filterState === "ALL") {
+            return true
+          } else {
+            return todo.status === filterState;
+          }
+        }).map((todo, index) => {
+          return <div id='Tasks' key={index}>
+            <div id='checkBox-text-container'>
+              <input id='checkBox' type="checkbox" checked={todo.status === "DONE"} onChange={() => handleCheckBox(todo.id)} />
+              {todo.text}
+            </div>
+            <button id='deleteBUTT'>Delete</button>
+          </div>
+        }
+        )}
         <p>No task yet</p>
 
       </div>
